f8d257f3f68e352ca4f72617b9194a90dd2742bc
maciaszczykm <maciaszczykm@icloud.com>
2017-03-01 19:37:31 +0100

    Finish writing thesis' project section

--- docs/thesis.lol
+++ docs/thesis.lol
@@ -1,3 +1,4 @@
 \contentsline {lstlisting}{\numberline {4.1}Kod oprogramowania mikrokontrolera}{30}
-\contentsline {lstlisting}{\numberline {4.2}Klasa implementuj\IeC {\k a}ca animacj\IeC {\k e}}{35}
-\contentsline {lstlisting}{\numberline {4.3}Klasa s\IeC {\l }u\IeC {\.z}\IeC {\k a}ca do rejestrowania animacji}{36}
+\contentsline {lstlisting}{\numberline {4.2}Klasa odpowiedzialna za przechwytywanie ekranu}{35}
+\contentsline {lstlisting}{\numberline {4.3}Klasa implementuj\IeC {\k a}ca animacj\IeC {\k e}}{38}
+\contentsline {lstlisting}{\numberline {4.4}Klasa s\IeC {\l }u\IeC {\.z}\IeC {\k a}ca do rejestrowania animacji}{39}
---
+++ resources/capturer.swift
@@ -0,0 +1,18 @@
+import Foundation
+import Cocoa
+
+class ScreenCapturer: LightController {
+    
+    func run(brightness: Double, smothness: Double) {
+        let image = CGDisplayCreateImage(screen.id)
+        for light in lights.lights {
+            let crop = image?.cropping(to: light.area)
+            let inputImage = CIImage(cgImage:crop!)
+            filter.setValue(inputImage, forKey: kCIInputImageKey)
+            filter.setValue(CIVector(cgRect: inputImage.extent), forKey: kCIInputExtentKey)
+            light.color.update(context: context, image: filter.outputImage!, brightness: brightness, smothness: smothness)
+        }
+        self.serialPort.send(lights: lights)
+    }
+    
+}
